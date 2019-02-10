c460ebc52fe575d75053cb2f7eee60f66fb440fb
sleeplesseditor <josh@bryson.me.uk>
2019-02-10 14:27:29 +0000

    ResultsCard updated and app credentials revised

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { DateRange, ReactiveBase } from '@appbaseio/reactivesearch';
+import { DateRange, DataSearch, NumberBox, RangeSlider, ReactiveBase, ResultCard } from '@appbaseio/reactivesearch';
 import './App.css';
 
 const credentials = require('./config').credentialsKey;
@@ -9,16 +9,25 @@ class App extends Component {
     return (
       <div className="container">
         <ReactiveBase
-          app="housing"
+          app="airbeds-test-app"
           credentials={credentials}
-          type="listing"
           theme={{
             primaryColor: '#FF3A4E'
           }}
         >
           <nav className="nav">
             <div className="title">AirBeds</div>
+            <DataSearch
+              componentId="SearchSensor"
+              dataField="name"
+              autosuggest={false}
+              placeholder="Search by house names"
+              iconPosition="left"
+              className="search"
+              highlight={true}
+            />
           </nav>
+          
           <div className="left-col">
             <DateRange 
               dataField="date_from"
@@ -26,8 +35,69 @@ class App extends Component {
               title="When"
               numberOfMonths={1}
               queryFormat="basic_date"
+              initialMonth={new Date('04-01-2017')}
+            />
+            <NumberBox
+              componentId="GuestSensor"
+              dataField="accommodates"
+              title="Guests"
+              defaultSelected={2}
+              labelPosition="right"
+              data={{
+                start: 1,
+                end: 16,
+              }}
+            />
+            <RangeSlider
+              componentId="PriceSensor"
+              dataField="price"
+              title="Price Range"
+              range={{
+                  start: 10,
+                  end: 250
+              }}
+              rangeLabels={{
+                  start: "$10",
+                  end: "$250"
+              }}
+              defaultSelected={{
+                  start: 10,
+                  end: 50
+              }}
+              stepValue={10}
+              react={{
+                  and: ["DateRangeSensor"]
+              }}
             />
           </div>
+
+          <ResultCard 
+            className="right-col"
+            componentId="SearchResult"
+            dataField="name"
+            size={12}
+            onData={data => ({
+                image: data.image,
+                title: data.name,
+                description: (
+                    <div>
+                        <div className="price">${data.price}</div>
+                        <p className="info">{data.room_type} · {data.accommodates} guests</p>
+                    </div>
+                ),
+                url: data.listing_url,
+            })}
+            pagination
+            react={{
+                and: ['SearchSensor', 'GuestSensor', 'PriceSensor', 'DateRangeSensor', 'search'],
+            }}
+            innerClass={{
+              resultStats: 'result-stats',
+              list: 'list',
+              listItem: 'list-item',
+              image: 'image',
+            }}
+          />
         </ReactiveBase>
       </div>
     );
